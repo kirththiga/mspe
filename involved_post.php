63cc993c5a97d4bf954fccfcdc3365585d4a4717
kirththiga <kirththiga.murugupillai@mail.mcgill.ca>
2015-07-08 16:46:43 -0400

    Update and rename involved_post.php to involved_post.py

--- involved_post.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-$firstName = _POST['firstname'];
-$LastName = _POST['lastname'];
-$msg = $firstName."".$lastName."has subscribed."
-mail("kirththiga.m@gmail.com", "Test Email", $msg);*/
-echo "Hello";
-?>
